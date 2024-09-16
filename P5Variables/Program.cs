a43711f59b29adfe315ba159aacbbe12e51483a0
Aztaaroth <jimmy.lundstein@gmail.com>
2024-09-16 15:06:54 +0200

    First roadblock overcome
    the first exercise I had problem with

--- P5Variables/Program.cs
+++ P5Variables/Program.cs
@@ -1,1 +1,24 @@
 ﻿
+
+// This is how to declare a variable, but not initializing it
+// string user;
+
+// Initialize it before using
+// user = "Jimmy"
+// Console.WriteLine(user);
+
+//  string user2 = "Jymie"
+//  Console.WriteLine(user2);
+
+
+string a = "Hello";
+// a Now contains Hello
+Console.WriteLine(a);
+a = "World";
+// a now caontains World
+Console.WriteLine(a);
+string b = a;
+Console.WriteLine(b);
+a = "forsbergs";
+Console.WriteLine(a);
+Console.WriteLine(b);
