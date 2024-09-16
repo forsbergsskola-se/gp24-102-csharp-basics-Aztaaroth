d744d915b4750fc65b813d9538fcd51c6cce92f8
Aztaaroth <jimmy.lundstein@gmail.com>
2024-09-16 13:40:25 +0200

    Second commit, half of first lecture

---
+++ P4Comments/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+// Single line comment.
+// Do a second single line comment.
+
+/* Now I can write as many lines as I want
+ * If I can spell
+ * poof.
+*/
+
+//Avoid spamming comments on codes
+//Make as descriptive comments as possible
+//Remember to update comments
+
+// Jimmy Lundstein
+// 16/09-24
+// jimmy.lundstein@forsbergsskola.se
+Console.WriteLine("Hello, World!");
