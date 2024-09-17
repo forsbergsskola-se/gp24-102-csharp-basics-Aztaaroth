ff73c8eb3e9c3b4776840085f7ad89bfd32e7560
Aztaaroth <jimmy.lundstein@gmail.com>
2024-09-17 14:24:27 +0200

    second huge wall, done

---
+++ P8Conversion/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+using System.Globalization;
+
+Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+string Question = "Give me a number";
+Console.WriteLine(Question);
+
+string input = Console.ReadLine();
+float numberAsDouble = float.Parse(input);
+
+Console.WriteLine(numberAsDouble);
+
+int i = (int) numberAsDouble;
+string finalNumber = i.ToString();
+
+Console.WriteLine(finalNumber);
