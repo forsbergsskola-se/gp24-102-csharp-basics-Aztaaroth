7e73111044dbd066da7d335fe367618781ac65e2
Aztaaroth <jimmy.lundstein@gmail.com>
2024-09-18 10:41:04 +0200

    brain mush hugest wall complete
    everything needed for G done

--- P9Operators/Program.cs
+++ P9Operators/Program.cs
@@ -1,1 +1,26 @@
 ﻿
+
+
+using System.Globalization; 
+Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture; 
+
+Console.WriteLine("Give me a number of seconds"); 
+string input = Console.ReadLine(); 
+
+int totalSeconds = int.Parse(input); 
+int seconds = totalSeconds % 60; 
+
+int totalMinutes = totalSeconds / 60; 
+int minutes = totalMinutes % 60; 
+
+int totalHours = totalMinutes / 60; 
+int hours = totalHours % 24; 
+
+int days = totalHours / 24; 
+
+Console.WriteLine($"Seconds: {seconds}"); 
+Console.WriteLine($"Minutes: {minutes}"); 
+Console.WriteLine($"Hours: {hours}"); 
+Console.WriteLine($"Days: {days}"); 
+Console.WriteLine($"{days}.{hours:D2}:{minutes:D2}:{seconds:D2}"); 
+Console.WriteLine($"In total, that's {days + (hours / 24.0) + (minutes / 1440.0) + (seconds / 86400.0)} Days.");
