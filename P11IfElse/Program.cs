e2b2a0d3f3371867eb0471d81bff07f98360cb3b
Aztaaroth <jimmy.lundstein@gmail.com>
2024-09-19 11:37:52 +0200

    start of P10 done with P11

---
+++ P11IfElse/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+Console.WriteLine("What is your age?");
+
+int age = int.Parse(Console.ReadLine());
+
+if (age < 13) { // 
+    Console.WriteLine("You're a child.");
+}else if (age < 18) {
+    Console.WriteLine("You're a teenager.");
+} else {
+    Console.WriteLine("You're an adult.");
+}
+
+Console.WriteLine("Give me another integer.");
+
+int wholeNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"The maximum is: {(age > wholeNumber ? age : wholeNumber)}.");
+
+int greaterNumber = age > wholeNumber ? age : wholeNumber;
+
+bool isEven = greaterNumber % 2 == 0; // 0 remainders == an even number.
+
+Console.WriteLine($"That number is an {(isEven ? "even" : "odd")} number.");
